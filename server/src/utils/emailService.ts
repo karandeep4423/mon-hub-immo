731f30b8f58001613d8eab337fc5920026d1e070
karandeep4423 <karanhanju9696@gmail.com>
2025-09-29 16:08:28 +0200

    email timeout error

--- server/src/utils/emailService.ts
+++ server/src/utils/emailService.ts
@@ -13,7 +13,7 @@ class EmailService {
 		this.transporter = nodemailer.createTransport({
 			host: process.env.EMAIL_HOST,
 			port: parseInt(process.env.EMAIL_PORT || '587'),
-			secure: false,
+			secure: true,
 			auth: {
 				user: process.env.EMAIL_USER,
 				pass: process.env.EMAIL_PASS,
