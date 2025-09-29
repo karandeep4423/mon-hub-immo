d550da44dace10d9b14fb45f4936475f96981d71
karandeep4423 <karanhanju9696@gmail.com>
2025-09-29 19:53:29 +0200

    email error prod

--- server/src/utils/emailService.ts
+++ server/src/utils/emailService.ts
@@ -12,7 +12,7 @@ class EmailService {
 	constructor() {
 		this.transporter = nodemailer.createTransport({
 			host: process.env.EMAIL_HOST,
-			port: parseInt(process.env.EMAIL_PORT || '587'),
+			port: parseInt(process.env.EMAIL_PORT || '465'),
 			secure: true,
 			auth: {
 				user: process.env.EMAIL_USER,
